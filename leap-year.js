972d1754fe6132d7d7d84013a7eebcc45149200c
Akshay Gupta <iitb.akshay@gmail.com>
2021-01-22 14:51:17 +0530

    Create leap-year.js

---
+++ leap-year.js
@@ -0,0 +1,16 @@
+/*
+
+  Is the given year a leap year?
+  
+  1991 => false
+  2000 => true
+  3000 => false
+  
+*/
+
+const fn(year) => year % 4 === 0
+                  ? (year % 100 === 0
+                    ? (year % 400 === 0 
+                      ? true : false)
+                    : true)
+                  : false;
