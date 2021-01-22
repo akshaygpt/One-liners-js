5b01e0fd0f4bf5802df5ae48f82878b1c9d29bf5
Akshay Gupta <iitb.akshay@gmail.com>
2021-01-22 14:41:31 +0530

    Update file-extension.js

--- file-extension.js
+++ file-extension.js
@@ -1,9 +1,10 @@
-/* Get file extension from file name
+/* 
 
-Example: 
-resume.pdf => .pdf
-webpack.config.js => .js
+  Get file extension from file name
 
+  resume.pdf => .pdf
+  webpack.config.js => .js
+  
 */
 
 const fn(s) => s.slice(s.lastIndexOf('.'));
