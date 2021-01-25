499ef00446dd8298277545d50fdc1376e18b8ef7
Akshay Gupta <iitb.akshay@gmail.com>
2021-01-25 14:38:09 +0530

    Update leap-year.js

--- leap-year.js
+++ leap-year.js
@@ -8,4 +8,4 @@
   
 */
 
-const isLeapYear(year) => year % 4 === 0 ? (year % 100 === 0 ? (year % 400 === 0 ? true : false) : true) : false;
+const fn = (year) => year % 4 === 0 ? (year % 100 === 0 ? (year % 400 === 0 ? true : false) : true) : false;
